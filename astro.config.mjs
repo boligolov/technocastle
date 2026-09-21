5f86240da4421d2e0e149cd77e816101937022d4
alisin <alexander.a.lisin@gmail.com>
2026-09-22 00:18:06 +0200

    * Astro update

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://technocastle.com',
+  // Preserve Astro 6 whitespace behavior (v7 default changed to 'jsx')
+  compressHTML: true,
   integrations: [sitemap()],
 });
